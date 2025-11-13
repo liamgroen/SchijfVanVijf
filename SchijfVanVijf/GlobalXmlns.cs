bc3e8af0c9ee34c67722a1fdd13b28ff89046756
liamgroen <gorro.81-estribos@icloud.com>
2025-11-13 14:02:44 +0100

    Create App
    See how to run app at:
    
    https://dotnet.microsoft.com/en-us/learn/maui/first-app-tutorial/run

---
+++ SchijfVanVijf/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SchijfVanVijf")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SchijfVanVijf.Pages")]
