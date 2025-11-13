d51a1ffad4e7961e12d3eceb1b47b1a1b85a0ac8
liamgroen <gorro.81-estribos@icloud.com>
2025-11-13 14:28:44 +0100

    Downgrade to .NET 8
    solve compatibility issue

--- SchijfVanVijf/App.xaml.cs
+++ SchijfVanVijf/App.xaml.cs
@@ -5,10 +5,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-	}
 
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
+		MainPage = new AppShell();
 	}
 }
