41ebf4fdb277085bfca4113b82b22668ced653b5
anasszaoujal <beargearmc@gmail.com>
2025-11-22 12:37:39 +0100

    Create Ingredients.cs

---
+++ SchijfVanVijf/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IDictionary<K, E>
+{
+    E remove(K key);
+    bool Add(K key, E value);
+}
+public class Recipe
+{
+    public Recipe()
+    {
+        Dictionary<Ingredient,int> recept = new Dictionary<Ingredient,int> ();
+        Recipe r;
+        string Instructions;
+    }
+
+}
