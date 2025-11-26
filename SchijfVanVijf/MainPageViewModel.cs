3ac387a2770cb69dde823bf1eb3271e1d6e33276
anasszaoujal <beargearmc@gmail.com>
2025-11-26 20:50:21 +0100

    fixed small error in mainpage

--- SchijfVanVijf/MainPageViewModel.cs
+++ SchijfVanVijf/MainPageViewModel.cs
@@ -8,7 +8,7 @@ namespace SchijfVanVijf;
 
 public class MainPageViewModel : INotifyPropertyChanged
 {
-    public event PropertyChangedEventHandler PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
     
     private readonly Database _database;
     
