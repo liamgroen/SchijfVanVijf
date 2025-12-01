ee23f59970f2b967015d8e572b3d5cf157e4ac7d
Tom Huisman <t.a.huisman1@students.uu.nl>
2025-12-01 12:37:28 +0100

    Extra comments
    Extra comments zodat iedereen makkelijker de code kan begrijpen

--- SchijfVanVijf/Data/Constants.cs
+++ SchijfVanVijf/Data/Constants.cs
@@ -4,17 +4,15 @@ namespace SchijfVanVijf.Data;
 
 public static class Constants
 {
-    public const string DatabaseFilename = "SchijfVanVijf.db3";
+    public const string DatabaseFilename = "SchijfVanVijf.db3"; // implementatie voor de naam voor de SQLite-databasebestand
 
-    public const SQLite.SQLiteOpenFlags Flags =
-        // open the database in read/write mode
-        SQLite.SQLiteOpenFlags.ReadWrite |
-        // create the database if it doesn't exist
-        SQLite.SQLiteOpenFlags.Create |
-        // enable multi-threaded database access
-        SQLite.SQLiteOpenFlags.SharedCache;
+    public const SQLite.SQLiteOpenFlags Flags = // flags die nodig zijn voor onze databaseintegratie
+        SQLite.SQLiteOpenFlags.ReadWrite | // opent de database in de nodige read/write modus
+        SQLite.SQLiteOpenFlags.Create | // maakt een database aan als deze nog niet bestaat
+        SQLite.SQLiteOpenFlags.SharedCache | // geeft toegang tot mulit-treaded toegang tot de database
+        SQLite.SQLiteOpenFlags.ProtectionComplete; // Het bestand is gecodeerd en ontoegankelijk zolang het apparaat is vergrendeld
 
     public static string DatabasePath =>
-        Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+        Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename); // maakt de map, verzorgd de bestandslocatie waar de database wordt opgeslagen
 
 }
--- SchijfVanVijf/Data/Database.cs
+++ SchijfVanVijf/Data/Database.cs
@@ -7,32 +7,32 @@ namespace SchijfVanVijf.Data;
 public class Database
 {
     //create database object used for interacting with the SQLite DB
-    private SQLiteAsyncConnection database;
-    private DatabaseSeeder seeder;
+    private SQLiteAsyncConnection database; // asyncConnetie met de database
+    private DatabaseSeeder seeder;// stopt bij het opstarten de data in de database
 
-    private async Task Init()
+    private async Task Init() // checkt of de databse al bestaar
     {
         if (database is not null)
         {
-            return;
+            return; // ga terug als de database bestaat
         }
 
-        database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-        await CreateTablesAsync();
-        await SeedDatabaseAsync();
+        database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags); // maak nieuwe connectie met SQLiteAsyncConnetie 
+        await CreateTablesAsync(); // zorgt dat talleben bestaan
+        await SeedDatabaseAsync(); // vult de tabellen met data
 
 
     }
 
     //Make sure that tables exist>
-    private async Task CreateTablesAsync()
+    private async Task CreateTablesAsync() // maakt tabellen aan 
     {
         await database.CreateTableAsync<Ingredient>();
         await database.CreateTableAsync<Recipe>();
         await database.CreateTableAsync<RecipeIngredient>();
     }
     
-    private async Task SeedDatabaseAsync()
+    private async Task SeedDatabaseAsync() // maakt een databaseSeeder
     {
         seeder = new DatabaseSeeder(database);
         await seeder.SeedAsync();
@@ -43,10 +43,10 @@ public class Database
     ///=========================
     public async Task<Ingredient> GetIngredientAsync(int Id)
     {
-        await Init();
-        return await database.Table<Ingredient>()
-            .Where(i => i.Id == Id)
-            .FirstOrDefaultAsync();
+        await Init(); // zorgt dtat de database klaarstaat
+        return await database.Table<Ingredient>() // vraagt ingrediënten tabel op
+            .Where(i => i.Id == Id) // filtert op id
+            .FirstOrDefaultAsync(); // geeft het eerstvolgende ingrediënt terug, of null
     }
     
     //TODO:
@@ -55,31 +55,31 @@ public class Database
     ///=========================
     public async Task<Recipe> GetRecipeAsync(int Id)
     {
-        await Init();
-        return await database.Table<Recipe>()
-            .Where(i => i.Id == Id)
-            .FirstOrDefaultAsync();
+        await Init(); // zorgt dtat de database klaarstaat
+        return await database.Table<Recipe>() // vraagt ingrediënten tabel op
+            .Where(i => i.Id == Id) // filtert op id
+            .FirstOrDefaultAsync(); // geeft het eerstvolgende ingrediënt terug, of null
     }
 
     public async Task<List<Recipe>> GetRecipeListAsync(List<int> Ids)
     {
-        if (Ids == null || Ids.Count == 0)
+        if (Ids == null || Ids.Count == 0) // Als lijst met ID's leeg is, return lege lijst
         {
             return new List<Recipe>();  
         }
 
         await Init();
-        return await database.Table<Recipe>()
+        return await database.Table<Recipe>() // alle recepten ophalen waar het ID in de lijst zit
             .Where(i => Ids.Contains(i.Id))
             .ToListAsync();
     }
 
-    
+
     //TODO:
     ///=========================
     /// RecipeIngredients CRUD 
     ///=========================
-    
+
 
     // ============================================
     // RECIPE SEARCH / FILTER
@@ -88,17 +88,19 @@ public class Database
     {
         await Init();
 
-        if (ingredientIds == null || ingredientIds.Count == 0)
-            return new List<Recipe>();
+        if (ingredientIds == null || ingredientIds.Count == 0) // Als lijst met ingredientsID's leeg is, return lege lijst
+        { 
+        return new List<Recipe>();
+        }   
 
-        var recipeIds = (await database.Table<RecipeIngredient>()
-            .Where(ri => ingredientIds.Contains(ri.IngredientId))
+        var recipeIds = (await database.Table<RecipeIngredient>() // zoekt in RecipeIngredient alle rijen waar IngredientId in de lijst zit
+            .Where(ri => ingredientIds.Contains(ri.IngredientId)) // zorgt ervoor dat er alleen rijen opver blijven één van de ingredientID lijst
             .ToListAsync())
-            .Select(ri => ri.RecipeId)
-            .Distinct()
+            .Select(ri => ri.RecipeId) // selecteer uit de gevonden koppelling alleen de RecipeId
+            .Distinct() // Verwijderd de dubbele RecipeId
             .ToList();
 
-        return await GetRecipeListAsync(recipeIds);
+        return await GetRecipeListAsync(recipeIds); // Haal de volledige Recipe-objecten op die horen bij het gevonden RecipeId
     }
 
 }
