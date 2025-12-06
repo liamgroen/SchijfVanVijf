1d82ead8c2f9f11d35fd783b4104ca9beaf08f41
Tessa Holzhaus <tessaholzhaus16@gmail.com>
2025-12-06 17:07:58 +0100

    Filter Page
    Adjusted the layout of the main filter page to match the recipe main page

--- SchijfVanVijf/Views/FilterMainPage.xaml.cs
+++ SchijfVanVijf/Views/FilterMainPage.xaml.cs
@@ -1,14 +1,36 @@
 using Microsoft.Maui.Platform;
 using SchijfVanVijf.ViewModel;
+using System.Collections.ObjectModel;
 
 namespace SchijfVanVijf.Views;
 
 public partial class FilterMainPage : ContentPage
 {
-	public FilterMainPage()
+    //Creates a observableCollection for the ingredient categories
+    public ObservableCollection<CategoryItems> CategoryList { get; set; } = new ObservableCollection<CategoryItems>();
+    public class CategoryItems
+    {
+        public string CategoryName { get; set; }
+    }
+    public FilterMainPage()
 	{
 		InitializeComponent();
-		//BindingContext = new ViewModel.FilterMainPageViewModel();
+        //BindingContext = new ViewModel.FilterMainPageViewModel();
+        BindingContext = this;
+        //Adds a few categories to the categorylist for testing
+        CategoryList.Add(new CategoryItems { CategoryName = "groenten" });
+        CategoryList.Add(new CategoryItems { CategoryName = "fruit" });
+        CategoryList.Add(new CategoryItems { CategoryName = "vlees" });
+        CategoryList.Add(new CategoryItems { CategoryName = "vis" });
+        CategoryList.Add(new CategoryItems { CategoryName = "vegetarisch" });
+        CategoryList.Add(new CategoryItems { CategoryName = "zuivel" });
+        CategoryList.Add(new CategoryItems { CategoryName = "ei" });
+        CategoryList.Add(new CategoryItems { CategoryName = "pasta" });
+        CategoryList.Add(new CategoryItems { CategoryName = "rijst" });
+        CategoryList.Add(new CategoryItems { CategoryName = "granen" });
+        CategoryList.Add(new CategoryItems { CategoryName = "kruiden" });
+        CategoryList.Add(new CategoryItems { CategoryName = "noten" });
+        CategoryList.Add(new CategoryItems { CategoryName = "overigen" });
     }
 
     //Uses the text on the button that is pressed by the user and hands it to the newly created filter selected page
