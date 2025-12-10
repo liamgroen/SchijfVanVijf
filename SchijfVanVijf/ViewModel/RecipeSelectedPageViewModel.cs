ff955a383a006d2dc30e34b578e4c95185d482a0
TessaHolzhaus <tessaholzhaus16@gmail.com>
2025-12-10 13:24:49 +0100

    Filter page UI update
    Finished basic UI for filter page and added a 'servings' button

--- SchijfVanVijf/ViewModel/RecipeSelectedPageViewModel.cs
+++ SchijfVanVijf/ViewModel/RecipeSelectedPageViewModel.cs
@@ -1,9 +1,77 @@
-﻿namespace SchijfVanVijf.ViewModel;
+﻿using System.ComponentModel;
+using System.Windows.Input;
 
-public class RecipeSelectedPageViewModel
+namespace SchijfVanVijf.ViewModel;
+
+public class RecipeSelectedPageViewModel : INotifyPropertyChanged
 {
-    public RecipeSelectedPageViewModel()
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public ICommand LessCommand { get; }
+    public ICommand MoreCommand { get; }
+    public string Recipe { get; set; }
+    private double _servingsAmount;
+    public double servingsAmount
+    {
+        get => _servingsAmount;
+        set
+        {
+            if (_servingsAmount != value)
+            {
+                _servingsAmount = value;
+                OnPropertyChanged(nameof(servingsAmount));
+            }
+        }
+    }
+
+    private string _servingsText;
+    public string servingsText
+    {
+        get => _servingsText;
+        set
+        {
+            if (_servingsText != value)
+            {
+                _servingsText = value;
+                OnPropertyChanged(nameof(servingsText));
+            }
+        }
+    }
+    public RecipeSelectedPageViewModel(string recipe)
     {
         //TODO: Split view into view and viewmodel
+
+        Recipe = recipe;
+        servingsAmount = 4;
+        _servingsText = $"{servingsAmount} servings";
+        LessCommand = new Command(LessServings);
+        MoreCommand = new Command(MoreServings);
+    }
+
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    
+    //TODO: Write OnScrollViewScrolled - Used to scroll through longer recipes
+    public async Task OnScrollViewScrolled(object sender, ScrolledEventArgs e)
+    {
+    }
+
+    public async void LessServings()
+    {
+        servingsAmount--;
+        if (servingsAmount < 1)
+        {
+            servingsAmount = 0.5;
+        }
+        servingsText = $"{servingsAmount} servings";
+    }
+
+    public async void MoreServings()
+    {
+        servingsAmount++;
+        if (servingsAmount == 1.5)
+        {
+            servingsAmount = 1;
+        }
+        servingsText = $"{servingsAmount} servings";
     }
 }
--- SchijfVanVijf/Views/RecipeSelectedPage.xaml.cs
+++ SchijfVanVijf/Views/RecipeSelectedPage.xaml.cs
@@ -1,16 +1,14 @@
+using System.Diagnostics;
 namespace SchijfVanVijf.Views;
 
 public partial class RecipeSelectedPage : ContentPage
 {
-    public string Recipe { get; set; }
     public RecipeSelectedPage(string recipe)
 	{
 		InitializeComponent();
-		Recipe = recipe;
-		BindingContext = this;
-	}
+        BindingContext = new ViewModel.RecipeSelectedPageViewModel(recipe);
 
-	public async void OnScrollViewScrolled (Object sender, ScrolledEventArgs e)
-	{   
     }
+
+
 }
